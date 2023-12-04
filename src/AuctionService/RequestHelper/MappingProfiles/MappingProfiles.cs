bfa39bfd6ae15d49bfcebac07c36edadfe0088f8
Per(k) <aupper094@gmail.com>
2023-12-04 11:18:28 +0700

    End simple crud auction

---
+++ src/AuctionService/RequestHelper/MappingProfiles/MappingProfiles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace Company.ClassLibrary1;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDto>()
+            .IncludeMembers(x => x.Item); // Include the Item properties in the AuctionDto
+        CreateMap<Item, AuctionDto>();
+        CreateMap<CreateAuctionDto, Auction>()
+            .ForMember(x => x.Item, opt => opt.MapFrom(src => src));
+        CreateMap<CreateAuctionDto, Item>();
+    }
+}
